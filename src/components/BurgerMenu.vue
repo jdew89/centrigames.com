c1a599d1d09aa4d64eee14b89dadb3ae2343a328
Jeff DeWitt <jadewitt00@gmail.com>
2022-06-27 12:25:45 -0400

    added menu icon animation

--- src/components/BurgerMenu.vue
+++ src/components/BurgerMenu.vue
@@ -1,17 +1,33 @@
 <script setup>
+import {ref} from 'vue';
 
+const menuActive = ref(false);
+
+function ToggleMenu() {
+    menuActive.value = !menuActive.value;
+}
 </script>
 
 <template>
-    <div class=" my-auto mx-2">
-        <div class="bar"></div>
-        <div class="bar"></div>
-        <div class="bar"></div>
+    <div class=" my-auto mx-2" @click="ToggleMenu">
+        <div class="bar" :class="{'bar1-active':menuActive}"></div>
+        <div class="bar" :class="{'bar2-active':menuActive}"></div>
+        <div class="bar" :class="{'bar3-active':menuActive}"></div>
     </div>
 </template>
 
-<style>
+<style scoped>
 .bar {
-    @apply w-9 h-1 bg-white my-1.5
+    @apply w-9 h-1 bg-white my-1.5 transition-all duration-150 ease-in rounded-lg;
+}
+
+.bar1-active {
+    @apply rotate-45 translate-y-2.5;
+}
+.bar2-active {
+    @apply opacity-0;
+}
+.bar3-active {
+    @apply -rotate-45 -translate-y-2.5;
 }
 </style>
