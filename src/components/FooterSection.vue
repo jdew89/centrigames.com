6bb9aa334016e2a3fdef534572f95164f9d92c35
Jeff DeWitt <jadewitt00@gmail.com>
2022-07-02 15:45:13 -0400

    footer setup

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -3,26 +3,37 @@
 </script>
 
 <template>
-    <footer class="text-center lg:text-left bg-primary text-brand-white font-sans bg-[#032730]">
+    <footer class=" bg-primary text-brand-white font-sans bg-primary-dark p-2 px-4 mt-2">
         <div class="container mx-auto">
-            <div class="grid grid-flow-col">
-                <div>
-                    <h3 class=" text-lg font-bold">Follow us on social Media</h3>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class=" stroke-[#FFFEEB]">
-                        <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
-                    </svg>
-                    <p>test</p>
-                    <p>test</p>
+            <div class="grid grid-flow-row md:grid-flow-col gap-6 lg:grid-cols-3 lg:w-10/12 xl:w-8/12 lg:mx-auto">
+                <div class="grid grid-flow-row gap-2">
+                    <h3 class=" text-lg font-bold">Follow us on social media</h3>
+                    <a class="flex flex-row gap-2" href="https://www.instagram.com/centrigames/" target="_blank" >
+                        <svg class=" stroke-brand-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" >
+                            <path fill="#FFFEEB" d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
+                        </svg>
+                        @centrigames
+                    </a>
+                    <a class="flex flex-row gap-2" href="https://twitter.com/search?q=centrigames&src=typed_query&f=user" target="_blank" >
+                        <svg class=" stroke-brand-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path fill="#FFFEEB" d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/>
+                        </svg>
+                        @centrigames
+                    </a>
                 </div>
-                <div>
-                    <h3 class=" text-lg font-bold">Links</h3>
-                    <p>test</p>
-                    <p>test</p>
+                <div class="grid grid-flow-row gap-1">
+                    <h3 class=" text-lg font-bold ">Navigation</h3>
+                    <a class="underline" href="/">Home</a>
+                    <a class="underline" href="/about">About</a>
+                    <a class="underline" href="#">Kickstarter Campaign</a>
                 </div>
-                <div class=" text-center">
-                    Copyright &#169; Centri Games LLC {{(new Date(Date.now())).getFullYear()}} All Rights Reserved
+                <div class=" text-center box-border flex mt-2 py-2">
+                    Subscribe to our news letter
                 </div>
             </div>
+        </div>
+        <div class=" text-center box-border border-t-2 flex items-center justify-center mt-2 py-2 text-xs border-y-gray-600">
+            Copyright &#169; Centri Games LLC {{(new Date(Date.now())).getFullYear()}} All Rights Reserved
         </div>
     </footer>
 </template>
