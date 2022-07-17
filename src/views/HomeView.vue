9d825a7979a1e6145dd0d67c2fba327e31a4bfc1
Jeff DeWitt <jadewitt00@gmail.com>
2022-07-17 16:30:20 -0400

    added subscript section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -32,7 +32,7 @@
     <!-- End old section -->
 
     <!-- New Header Section -->
-    <section class=" bg-primary-dark">
+    <section class=" bg-primary-dark pt-11 mb-4">
       <div class="flex md:flex-row flex-col items-center justify-center">
         <div class=" basis-1/2">
           <img src="../assets/Instagram_5x5_art_name.jpg" alt="Ruthless Merchants - The rowdy marketplace full of ruthless encounters" class="aspect-auto w-max h-auto mx-auto">
@@ -48,8 +48,8 @@
         </div>
       </div>
     </section>
-    <section>
-      <div class="container p-2 my-2 text-lg">
+    <section class="mb-4">
+      <div class="container p-2 my-2 text-lg mx-auto">
         <p class=" font-sans mb-2 text-lg">
           Ruthless Merchants is a marketplace full of cunning and unruly characters. The goal of the game is simple: collect coins. But, why is it a “ruthless” marketplace, you ask? Many actions have devious intentions.
         </p>
@@ -58,14 +58,27 @@
         </p>
       </div>
     </section>
-    <section class="container p-2 bg-primary-dark text-brand-white">
-      <div class="">
-        <blockquote class="text-lg tracking-widest big-quote relative p-4 pt-8">
+    <section class="bg-primary-dark mb-4 px-4 pt-10 pb-14 md:pb-10 font-sans text-brand-white">
+      <div class="container mx-auto ">
+        <div class=" text-center md:flex md:justify-center md:items-center">
+          <label for="emailsubscribe" class="text-2xl font-bold md:w-1/3">Subscribe to our newsletter</label>
+          <div class="relative pt-2 md:w-2/3">
+              <input id="emailsubscribe" name="emailsubscribe" type="email" placeholder="email@example.com" class="text-black w-full h-10 pl-2 pr-20 rounded-lg focus:shadow focus:outline-none">
+              <div class="absolute top-3 right-1">
+                  <button @submit.prevent="" class="h-8 w-20 text-white font-bold rounded-lg bg-primary hover:bg-primary-dark" type="submit">Subscribe</button>
+              </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class=" p-2">
+      <div class="container mx-auto">
+        <blockquote class="text-lg tracking-widest big-quote relative p-4 pt-9">
           A ferociously fun spin on the pick up and deliver genre. Easy to learn and quick to play, 
           you will enjoy bluffing your friends and family while attempting to deliver your hard earned goods. 
           Good luck trying to dodge their punches and thrown rocks! 
           If you don't mind a little bit of conflict at your table, you should check this game out!
-          <cite class=" before:content-['-'] block text-gray-300 text-base">
+          <cite class=" before:content-['-'] block text-gray-600 text-base">
             Ben Strouse (ArtemisX on BGG)
           </cite>
         </blockquote>
@@ -90,17 +103,8 @@
   @apply font-sans text-lg lg:text-2xl;
 }
 
-.test{
-  /*background-image: url(/src/assets/blob-scene-haikei.svg);*/
-  background-image: url(/src/assets/polygon-scatter-haikei-1.svg);
-  background-color: #0d9bbf;
-  background-repeat: no-repeat;
-  background-size: cover;
-  width: 100%;
-}
-
 .big-quote {
-  @apply before:content-['\201C'] before:block before:text-8xl before:absolute before:-left-3 before:-top-2 before:text-primary;
+  @apply before:content-['\201C'] before:block before:text-8xl before:absolute before:-left-3 before:-top-1 before:text-primary-dark;
 }
 
 
